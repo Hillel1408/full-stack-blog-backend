c7c4c6f5114d101ac025cdc058a4e400bb063f72
Hillel1408 <etemax@bk.ru>
2022-09-02 14:12:03 +0300

    final

---
+++ utils/handleValidationErrors.js
@@ -0,0 +1,11 @@
+import validator from 'express-validator';
+const { validationResult } = validator;
+
+export default (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
+
+    next();
+};
